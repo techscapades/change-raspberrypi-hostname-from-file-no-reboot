587410e82888729051a41cca1e7ab5dc0077e9c1
Techscapades <76870663+techscapades@users.noreply.github.com>
2025-02-04 20:04:06 +0800

    Add files via upload

--- change_hostname.sh
+++ change_hostname.sh
@@ -1,31 +1,12 @@
 #!/bin/bash
 
-# Define the expected file path
-file_path="/cam_name"
-
-# Check if the file exists at root
-if [ -f "$file_path" ]; then
-    # Read the value from the file
-    value=$(< "$file_path")
-    echo "The value from $file_path is: $value"
-else
-    echo "Error: File $file_path does not exist at root!"
-    exit 1
-fi
-
-
-echo "The value from the cam_name file is: $value"
-echo "changing hostname to $value.local"
-echo "$value" | tee /etc/new_hostname.txt > /dev/null
-
 set +e
 
 CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
-NEW_HOSTNAME=`cat /etc/new_hostname.txt | tr -d " \t\n\r"`
+NEW_HOSTNAME=`cat /path_to_new_hostname_file | tr -d " \t\n\r"`
 
-echo `cat /etc/new_hostname.txt | tr -d "\t\n\r"` >/etc/hostname
+echo `cat /path_to_new_hostname_file | tr -d "\t\n\r"` >/etc/hostname
 sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
-echo `cat /etc/new_hostname.txt | tr -d "\t\n\r"` >/proc/sys/kernel/hostname
+echo `cat /etc/new_hostname | tr -d "\t\n\r"` >/proc/sys/kernel/hostname
 
 exit 0
-
